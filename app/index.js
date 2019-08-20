3f9dda458b2d2635c2d3cc6d033723f8c5218643
Matija Simin <marcussimon12082015.hotmail.com>
2019-08-20 11:15:57 -0700

    Done

--- app/index.js
+++ app/index.js
@@ -5,15 +5,33 @@ const express = require('express');
 const knex = require('knex');
 const handlebars = require('express-handlebars');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+
 const ENV = process.env.NODE_ENV || 'development';
 const config = require('../knexfile');
 const db = knex(config[ENV]);
 
 // Initialize Express.
 const app = express();
+app.use(flash());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session({secret: 'our secret string'}));
+app.use(cookieParser());
 app.use(passport.initialize());
 
+
+const isAuthenticated = (req, res, done) => {
+  if (req.session && req.session.passport) {
+    return done();
+  }
+  res.redirect('/login');
+};
 // Configure handlebars templates.
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
@@ -31,11 +49,57 @@ const Comment = require('./models/comment');
 const Post = require('./models/post');
 const User = require('./models/user');
 
+passport.use(new LocalStrategy((username, password, done) => {
+  User
+    .forge({ username: username })
+    .fetch()
+    .then((usr) => {
+      if (!usr) {
+        return done(null, false);
+      }
+      usr.validatePassword(password).then((valid) => {
+        if (!valid) {
+          return done(null, false);
+        }
+        return done(null, usr);
+      });
+    })
+    .catch((err) => {
+      return done(err);
+    });
+}));
 
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function(user, done) {
+  User
+    .forge({id: user})
+    .fetch()
+    .then((usr) => {
+      done(null, usr);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
 
 // ***** Server ***** //
+
+app.get('/login', (req, res) => {
+  res.render('login', { message: req.flash('error) });
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true
+  }),
+  function(req, res) {
+    res.redirect('/posts');
+  });
 
 app.get('/user/:id', (req,res) => {
   User
@@ -67,7 +131,7 @@ app.post('/user', (req, res) => {
     });
 });
 
-app.get('/posts', (req, res) => {
+app.get('/posts', isAuthenticated, (req, res) => {
   Post
     .collection()
     .fetch()
